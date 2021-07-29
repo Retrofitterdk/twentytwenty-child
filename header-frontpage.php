9c48a46f08f8c0a2a4ddaf4e3b2b18e246cdeb78
Retrofitterdk <sbn@retrofitter.dk>
2021-07-29 11:44:53 +0200

    misc css changes

--- header-frontpage.php
+++ header-frontpage.php
@@ -32,6 +32,8 @@
 
 		<header id="site-header" class="header-footer-group" role="banner">
 
+		<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/551845977?autoplay=1&loop=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+
 			<div class="header-inner section-inner">
 
 				<div class="header-titles-wrapper">
